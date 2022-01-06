64540e4169079f7276ccc12e6ef2913b9e15e598
gmendozah <geovanimendoza.h@gmail.com>
2022-01-06 15:12:17 -0400

    feat: solve gregorian y

---
+++ hackerrank_exercises/python_introduction/gregorian_year.py
@@ -0,0 +1,11 @@
+def is_leap(year):
+    leap = False
+
+    # Write your logic here
+    if (year % 4 == 0 and year % 100 != 0) or (year % 100 == 0 and year % 400 == 0):
+        leap = True
+
+    return leap
+
+
+print(is_leap(2100))
